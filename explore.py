d82bd3078bd340d0ba5148e46cabf71f98f9ef21
Justin Sullivan <sullivan.justin.michael@gmail.com>
2021-04-09 18:01:13 -0500

    functions for exploration

---
+++ explore.py
@@ -0,0 +1,186 @@
+import pandas as pd
+import numpy as np
+import acquire
+import prepare
+import sklearn.preprocessing
+from sklearn.model_selection import train_test_split
+from scipy import stats
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def explore_univariate(train, cat_vars, quant_vars):
+    '''
+    explore each individual categorical variably by: 
+    taking in a dataframe and a categorical variable and returning
+    a frequency table and barplot of the frequencies. 
+    
+    explore each individual quantitative variable by:
+    takes in a dataframe and a quantitative variable and returns
+    descriptive stats table, histogram, and boxplot of the distributions. 
+    '''
+    for var in cat_vars:
+        explore_univariate_categorical(train, var)
+        print('_________________________________________________________________')
+    for col in quant_vars:
+        p, descriptive_stats = explore_univariate_quant(train, col)
+        plt.show(p)
+        print(descriptive_stats)
+
+def explore_univariate_categorical(train, cat_var):
+    '''
+    takes in a dataframe and a categorical variable and returns
+    a frequency table and barplot of the frequencies. 
+    '''
+    frequency_table = freq_table(train, cat_var)
+    plt.figure(figsize=(2,2))
+    sns.barplot(x=cat_var, y='Count', data=frequency_table, color='lightseagreen')
+    plt.title(cat_var)
+    plt.show()
+    print(frequency_table)
+
+def explore_univariate_quant(train, quant_var):
+    '''
+    takes in a dataframe and a quantitative variable and returns
+    descriptive stats table, histogram, and boxplot of the distributions. 
+    '''
+    descriptive_stats = train[quant_var].describe()
+    plt.figure(figsize=(8,2))
+
+    p = plt.subplot(1, 2, 1)
+    p = plt.hist(train[quant_var], color='lightseagreen')
+    p = plt.title(quant_var)
+
+    # second plot: box plot
+    p = plt.subplot(1, 2, 2)
+    p = plt.boxplot(train[quant_var])
+    p = plt.title(quant_var)
+    return p, descriptive_stats
+    
+def freq_table(train, cat_var):
+    '''
+    for a given categorical variable, compute the frequency count and percent split
+    and return a dataframe of those values along with the different classes. 
+    '''
+    class_labels = list(train[cat_var].unique())
+
+    frequency_table = (
+        pd.DataFrame({cat_var: class_labels,
+                      'Count': train[cat_var].value_counts(normalize=False), 
+                      'Percent': round(train[cat_var].value_counts(normalize=True)*100,2)}
+                    )
+    )
+    return frequency_table
+
+######
+
+#####
+
+###Bivariate
+
+def explore_bivariate(train, categorical_target, continuous_target, binary_vars, quant_vars):
+    '''
+    This function makes use of explore_bivariate_categorical and explore_bivariate_quant functions. 
+    Each of those take in a continuous target and a binned/cut version of the target to have a categorical target. 
+    the categorical function takes in a binary independent variable and the quant function takes in a quantitative 
+    independent variable. 
+    '''
+    for binary in binary_vars:
+        explore_bivariate_categorical(train, categorical_target, continuous_target, binary)
+    for quant in quant_vars:
+        explore_bivariate_quant(train, categorical_target, continuous_target, quant)
+
+###################### ________________________________________
+## Bivariate Categorical
+
+def explore_bivariate_categorical(train, categorical_target, continuous_target, binary):
+    '''
+    takes in binary categorical variable and binned/categorical target variable, 
+    returns a crosstab of frequencies
+    runs a chi-square test for the proportions
+    and creates a barplot, adding a horizontal line of the overall rate of the binary categorical variable. 
+    '''
+    print(binary, "\n_____________________\n")
+    
+    ct = pd.crosstab(train[binary], train[categorical_target], margins=True)
+    chi2_summary, observed, expected = run_chi2(train, binary, categorical_target)
+    mannwhitney = compare_means(train, continuous_target, binary, alt_hyp='two-sided')
+    p = plot_cat_by_target(train, categorical_target, binary)
+    
+    print("\nMann Whitney Test Comparing Means: ", mannwhitney)
+    print(chi2_summary)
+#     print("\nobserved:\n", ct)
+    print("\nexpected:\n", expected)
+    plt.show(p)
+    print("\n_____________________\n")
+    
+
+    
+def run_chi2(train, binary, categorical_target):
+    observed = pd.crosstab(train[binary], train[categorical_target])
+    chi2, p, degf, expected = stats.chi2_contingency(observed)
+    chi2_summary = pd.DataFrame({'chi2': [chi2], 'p-value': [p], 
+                                 'degrees of freedom': [degf]})
+    expected = pd.DataFrame(expected)
+    return chi2_summary, observed, expected
+
+
+def plot_cat_by_target(train, categorical_target, binary):
+    p = plt.figure(figsize=(2,2))
+    p = sns.barplot(categorical_target, binary, data=train, alpha=.8, color='lightseagreen')
+    overall_rate = train[binary].mean()
+    p = plt.axhline(overall_rate, ls='--', color='gray')
+    return p
+
+    
+def compare_means(train, continuous_target, binary, alt_hyp='two-sided'):
+    x = train[train[binary]==0][continuous_target]
+    y = train[train[binary]==1][continuous_target]
+    return stats.mannwhitneyu(x, y, use_continuity=True, alternative=alt_hyp)
+
+###################### ________________________________________
+## Bivariate Quant
+
+def explore_bivariate_quant(train, categorical_target, continuous_target, quant):
+    '''
+    descriptive stats by each target class. 
+    compare means across 2 target groups 
+    boxenplot of target x quant
+    swarmplot of target x quant
+    '''
+    print(quant, "\n____________________\n")
+    descriptive_stats = train.groupby(categorical_target)[quant].describe().T
+    spearmans = compare_relationship(train, continuous_target, quant)
+    plt.figure(figsize=(4,4))
+    boxen = plot_boxen(train, categorical_target, quant)
+#     swarm = plot_swarm(train, categorical_target, quant)
+    plt.show()
+    scatter = plot_scatter(train, categorical_target, continuous_target, quant)
+    plt.show()
+    print(descriptive_stats, "\n")
+    print("\nSpearman's Correlation Test:\n", spearmans)
+    print("\n____________________\n")
+
+
+def compare_relationship(train, continuous_target, quant):
+    return stats.spearmanr(train[quant], train[continuous_target], axis=0)
+
+def plot_swarm(train, categorical_target, quant):
+    average = train[quant].mean()
+    p = sns.swarmplot(data=train, x=categorical_target, y=quant, color='lightgray')
+    p = plt.title(quant)
+    p = plt.axhline(average, ls='--', color='black')
+    return p
+
+def plot_boxen(train, categorical_target, quant):
+    average = train[quant].mean()
+    p = sns.boxenplot(data=train, x=categorical_target, y=quant, color='lightseagreen')
+    p = plt.title(quant)
+    p = plt.axhline(average, ls='--', color='black')
+    return p
+
+def plot_scatter(train, categorical_target, continuous_target, quant):
+    p = sns.scatterplot(x=quant, y=continuous_target, hue=categorical_target, data=train)
+    p = plt.title(quant)
+    return p
